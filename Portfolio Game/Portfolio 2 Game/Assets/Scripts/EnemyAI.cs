eab97b06b6a39d79065be43c8636c550420af2d0
Austin <cap32014@gmail.com>
2025-03-10 16:31:00 -0500

    made a skeleton mostly

--- Portfolio Game/Portfolio 2 Game/Assets/Scripts/EnemyAI.cs
+++ Portfolio Game/Portfolio 2 Game/Assets/Scripts/EnemyAI.cs
@@ -1,28 +1,87 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.AI;
+using System.Runtime.CompilerServices;
 
-
-
-public class EnemyAI : MonoBehaviour, IDamage
+public class enemyAI : MonoBehaviour, IDamage
 {
     [SerializeField] Renderer model;
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Animator anim;
 
-    [Range(1, 10)][SerializeField] int HP;
+    [SerializeField] int HP;
+    [SerializeField] int faceTargetSpeed;
+    [SerializeField] int animTransSpeed;
 
-  
+    [SerializeField] Transform shootPos;
+    [SerializeField] GameObject bullet;
+    [SerializeField] float shootRate;
 
-    
+    float shootTimer;
+
+    Vector3 playerDir;
+    bool playerInRage;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        gamemanager.instance.updateGameGoal(1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        setAnimLocomotion();
+
+        shootTimer += Time.deltaTime;
+
+        if (playerInRage)
+        {
+            playerDir = gamemanager.instance.player.transform.position - transform.position;
+            agent.SetDestination(gamemanager.instance.player.transform.position);
+
+
+
+            if (shootTimer >= shootRate)
+            {
+                shoot();
+            }
+
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                faceTarget();
+            }
+        }
+    }
+
+    void setAnimLocomotion()
+    {
+        float agentSpeed = agent.velocity.normalized.magnitude;
+        float animSpeedCur = anim.GetFloat("Speed");
+        anim.SetFloat("Speed", Mathf.Lerp(animSpeedCur, agentSpeed, Time.deltaTime * animTransSpeed));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRage = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRage = false;
+        }
+    }
+
+    void faceTarget()
+    {
+        Quaternion rot = Quaternion.LookRotation(new Vector3(playerDir.x, transform.position.y, playerDir.z));
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * faceTargetSpeed);
     }
 
     public void takeDamage(int amount)
@@ -30,18 +89,24 @@ public class EnemyAI : MonoBehaviour, IDamage
         HP -= amount;
         StartCoroutine(flashRed());
 
-        if ((HP<= 0))
+        agent.SetDestination(gamemanager.instance.player.transform.position);
+
+        if (HP <= 0)
         {
-            {
-                Destroy(gameObject);
-            }
+            gamemanager.instance.updateGameGoal(-1);
+            Destroy(gameObject);
         }
     }
-
     IEnumerator flashRed()
     {
         model.material.color = Color.red;
         yield return new WaitForSeconds(0.1f);
         model.material.color = Color.white;
+    }
+
+    void shoot()
+    {
+        shootTimer = 0;
+        Instantiate(bullet, shootPos.position, transform.rotation);
     }
 }
