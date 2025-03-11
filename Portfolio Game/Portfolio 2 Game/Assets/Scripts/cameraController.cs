c8e96c168d5d193fd3f88820de9101cd7edadc87
TheGreenCow <alectamez@protonmail.com>
2025-03-10 17:50:44 -0700

    Repo Cleanup - Alec Tamez
    I will discard all changes from this branch.

---
+++ Portfolio Game/Portfolio 2 Game/Assets/Scripts/cameraController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get input
+
+        // Rotate the camera on the x-axis to look up and down
+
+        // Rotate the player on the y-axis to look left and right
+        
+    }
+}
