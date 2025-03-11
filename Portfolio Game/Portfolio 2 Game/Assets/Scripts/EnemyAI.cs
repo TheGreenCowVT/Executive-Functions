c44f8e63e0cb7303c6f8bff717e1a9ccaed563d0
TheGreenCow <alectamez@protonmail.com>
2025-03-10 18:05:13 -0700

    Squashed commit of the following:
    commit 3e89f7ce20fa3e1774a65e332fadda4f3fa295d8
    Author: Austin <cap32014@gmail.com>
    Date:   Mon Mar 10 19:59:12 2025 -0500
    
        beholder start of creation
    
    commit eab97b06b6a39d79065be43c8636c550420af2d0
    Author: Austin <cap32014@gmail.com>
    Date:   Mon Mar 10 16:31:00 2025 -0500
    
        made a skeleton mostly
    
    commit fff44391c7dd05f8e26be09e15e5b3dc09dd919c
    Author: Austin <cap32014@gmail.com>
    Date:   Sat Mar 8 22:37:24 2025 -0600
    
        day one of making enemies
    
    commit deb45dd0e11c3959574214bc8af8a898996f6040
    Author: Austin <cap32014@gmail.com>
    Date:   Fri Mar 7 20:34:15 2025 -0600
    
        First commit

---
+++ Portfolio Game/Portfolio 2 Game/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,112 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+using System.Runtime.CompilerServices;
+
+public class enemyAI : MonoBehaviour, IDamage
+{
+    [SerializeField] Renderer model;
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Animator anim;
+
+    [SerializeField] int HP;
+    [SerializeField] int faceTargetSpeed;
+    [SerializeField] int animTransSpeed;
+
+    [SerializeField] Transform shootPos;
+    [SerializeField] GameObject bullet;
+    [SerializeField] float shootRate;
+
+    float shootTimer;
+
+    Vector3 playerDir;
+    bool playerInRage;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gamemanager.instance.updateGameGoal(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        setAnimLocomotion();
+
+        shootTimer += Time.deltaTime;
+
+        if (playerInRage)
+        {
+            playerDir = gamemanager.instance.player.transform.position - transform.position;
+            agent.SetDestination(gamemanager.instance.player.transform.position);
+
+
+
+            if (shootTimer >= shootRate)
+            {
+                shoot();
+            }
+
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                faceTarget();
+            }
+        }
+    }
+
+    void setAnimLocomotion()
+    {
+        float agentSpeed = agent.velocity.normalized.magnitude;
+        float animSpeedCur = anim.GetFloat("Speed");
+        anim.SetFloat("Speed", Mathf.Lerp(animSpeedCur, agentSpeed, Time.deltaTime * animTransSpeed));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRage = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRage = false;
+        }
+    }
+
+    void faceTarget()
+    {
+        Quaternion rot = Quaternion.LookRotation(new Vector3(playerDir.x, transform.position.y, playerDir.z));
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * faceTargetSpeed);
+    }
+
+    public void TakeDamage(int amount)
+    {
+        HP -= amount;
+        StartCoroutine(flashRed());
+
+        agent.SetDestination(gamemanager.instance.player.transform.position);
+
+        if (HP <= 0)
+        {
+            gamemanager.instance.updateGameGoal(-1);
+            Destroy(gameObject);
+        }
+    }
+    IEnumerator flashRed()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = Color.white;
+    }
+
+    void shoot()
+    {
+        shootTimer = 0;
+        Instantiate(bullet, shootPos.position, transform.rotation);
+    }
+}
---
+++ Portfolio Game/Portfolio 2 Game/Assets/Scripts/gamemanager.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class gamemanager : MonoBehaviour
+{
+    public static gamemanager instance;
+
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPause;
+    [SerializeField] GameObject menuWin;
+    [SerializeField] GameObject menuLose;
+    [SerializeField] TMP_Text goalCountText;
+
+
+    public Image playerHPBar;
+    public GameObject playerDamageScreen;
+    public GameObject player;
+    public PlayerController playerScript;
+
+    public bool isPaused;
+
+    int goalCount;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        instance = this;
+        player = GameObject.FindWithTag("Player");
+        playerScript = player.GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (menuActive == null)
+            {
+                statePause();
+                menuActive = menuPause;
+                menuActive.SetActive(true);
+
+            }
+            else if (menuActive == menuPause)
+            {
+                stateUnpaused();
+            }
+        }
+    }
+
+    public void statePause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void stateUnpaused()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 1;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        menuActive.SetActive(false);
+        menuActive = null;
+
+    }
+
+    public void updateGameGoal(int amount)
+    {
+        goalCount += amount;
+        goalCountText.text = goalCount.ToString("F0");
+
+        if (goalCount <= 0)
+        {
+            // you win!!!
+            statePause();
+            menuActive = menuWin;
+            menuActive.SetActive(true);
+        }
+    }
+
+    public void youLose()
+    {
+        statePause();
+        menuActive = menuLose;
+        menuActive.SetActive(true);
+    }
+}
