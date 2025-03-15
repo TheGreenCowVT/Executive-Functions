a8c231068e7178f9923d163d790724b049aa0d1b
TheGreenCow <alectamez@protonmail.com>
2025-03-14 20:36:08 -0700

    Adding V2 Script for EnemyAi
    This script will be used for future iteration of chaseplayer(), attackplayer(), randomized waypoints, and using wire spheres to detect player.

--- Portfolio Game/Portfolio 2 Game/Assets/Scripts/EnemyAIV2.cs
+++ Portfolio Game/Portfolio 2 Game/Assets/Scripts/EnemyAIV2.cs
@@ -1,145 +1,145 @@
-using UnityEngine;
-using System.Collections;
-using UnityEngine.AI;
+//using UnityEngine;
+//using System.Collections;
+//using UnityEngine.AI;
 
-public class enemyAI : MonoBehaviour, IDamage
-{
-    [SerializeField] Renderer model;
-    [SerializeField] NavMeshAgent agent;
-    [SerializeField] Animator animator;
+//public class enemyAI : MonoBehaviour, IDamage
+//{
+//    [SerializeField] Renderer model;
+//    [SerializeField] NavMeshAgent agent;
+//    [SerializeField] Animator animator;
 
-    [SerializeField] int HP;
-    [SerializeField] int faceTargetSpeed;
-    [SerializeField] int animTransSpeed;
+//    [SerializeField] int HP;
+//    [SerializeField] int faceTargetSpeed;
+//    [SerializeField] int animTransSpeed;
 
-    [SerializeField] Transform shootPos;
-    [SerializeField] GameObject bullet;
-    [SerializeField] float shootRate;
-
-
-    int maxHP;
-    float shootTimer;
-
-    Vector3 playerDir;
-    bool playerInRage;
-    private EnemyLoot loot;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        loot = GetComponent<EnemyLoot>();
-        gamemanager.instance.updateGameGoal(1);
-        maxHP = HP;
-        updateenemyUI();
-    }
-    // Update is called once per frame
-    void Update()
-    {
+//    [SerializeField] Transform shootPos;
+//    [SerializeField] GameObject bullet;
+//    [SerializeField] float shootRate;
 
 
-        setAnimLocomotion();
-        updateenemyUI();
-        shootTimer += Time.deltaTime;
+//    int maxHP;
+//    float shootTimer;
 
-        if (playerInRage)
-        {
-            playerDir = gamemanager.instance.player.transform.position - transform.position;
-            agent.SetDestination(gamemanager.instance.player.transform.position);
+//    Vector3 playerDir;
+//    bool playerInRage;
+//    private EnemyLoot loot;
 
-
-
-            if (shootTimer >= shootRate)
-            {
-                shoot();
-            }
-
-            if (agent.remainingDistance <= agent.stoppingDistance)
-            {
-                faceTarget();
-            }
-        }
-    }
-
-    void setAnimLocomotion()
-    {
-        float agentSpeed = agent.velocity.normalized.magnitude;
-        float animSpeedCur = animator.GetFloat("Speed");
-        animator.SetFloat("Speed", Mathf.Lerp(animSpeedCur, agentSpeed, Time.deltaTime * animTransSpeed));
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            playerInRage = true;
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            playerInRage = false;
-        }
-    }
-
-    void faceTarget()
-    {
-        Quaternion rot = Quaternion.LookRotation(new Vector3(playerDir.x, transform.position.y, playerDir.z));
-        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * faceTargetSpeed);
-    }
-
-    public void TakeDamage(int amount)
-    {
-        if (HP > 0)
-        {
-            HP -= amount;
-        }
-        else
-        {
-            gamemanager.instance.updateGameGoal(-1);
-            loot.Die();
-
-            if (gameObject)
-            {
-                Destroy(gameObject);
-            }
+//    // Start is called once before the first execution of Update after the MonoBehaviour is created
+//    void Start()
+//    {
+//        loot = GetComponent<EnemyLoot>();
+//        gamemanager.instance.updateGameGoal(1);
+//        maxHP = HP;
+//        updateenemyUI();
+//    }
+//    // Update is called once per frame
+//    void Update()
+//    {
 
 
-        }
+//        setAnimLocomotion();
+//        updateenemyUI();
+//        shootTimer += Time.deltaTime;
 
-        updateenemyUI();
-        StartCoroutine(flashRed());
+//        if (playerInRage)
+//        {
+//            playerDir = gamemanager.instance.player.transform.position - transform.position;
+//            agent.SetDestination(gamemanager.instance.player.transform.position);
 
 
-        agent.SetDestination(gamemanager.instance.player.transform.position);
 
-        //if (HP <= 0)
-        //{
+//            if (shootTimer >= shootRate)
+//            {
+//                shoot();
+//            }
 
-        //    gamemanager.instance.updateGameGoal(-1);
-        //    loot.Die();
-        //}
-        //else
-        //{
-        //    Destroy(gameObject);
-        //}
-    }
-    IEnumerator flashRed()
-    {
-        model.material.color = Color.red;
-        yield return new WaitForSeconds(0.1f);
-        model.material.color = Color.white;
-    }
+//            if (agent.remainingDistance <= agent.stoppingDistance)
+//            {
+//                faceTarget();
+//            }
+//        }
+//    }
 
-    void shoot()
-    {
-        shootTimer = 0;
-        Instantiate(bullet, shootPos.position, transform.rotation);
-    }
+//    void setAnimLocomotion()
+//    {
+//        float agentSpeed = agent.velocity.normalized.magnitude;
+//        float animSpeedCur = animator.GetFloat("Speed");
+//        animator.SetFloat("Speed", Mathf.Lerp(animSpeedCur, agentSpeed, Time.deltaTime * animTransSpeed));
+//    }
 
-    public void updateenemyUI()
-    {
-        gamemanager.instance.enemyHPBar.fillAmount = (float)HP / maxHP;
-    }
-}
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        if (other.CompareTag("Player"))
+//        {
+//            playerInRage = true;
+//        }
+//    }
+
+//    private void OnTriggerExit(Collider other)
+//    {
+//        if (other.CompareTag("Player"))
+//        {
+//            playerInRage = false;
+//        }
+//    }
+
+//    void faceTarget()
+//    {
+//        Quaternion rot = Quaternion.LookRotation(new Vector3(playerDir.x, transform.position.y, playerDir.z));
+//        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * faceTargetSpeed);
+//    }
+
+//    public void TakeDamage(int amount)
+//    {
+//        if (HP > 0)
+//        {
+//            HP -= amount;
+//        }
+//        else
+//        {
+//            gamemanager.instance.updateGameGoal(-1);
+//            loot.Die();
+
+//            if (gameObject)
+//            {
+//                Destroy(gameObject);
+//            }
+
+
+//        }
+
+//        updateenemyUI();
+//        StartCoroutine(flashRed());
+
+
+//        agent.SetDestination(gamemanager.instance.player.transform.position);
+
+//        //if (HP <= 0)
+//        //{
+
+//        //    gamemanager.instance.updateGameGoal(-1);
+//        //    loot.Die();
+//        //}
+//        //else
+//        //{
+//        //    Destroy(gameObject);
+//        //}
+//    }
+//    IEnumerator flashRed()
+//    {
+//        model.material.color = Color.red;
+//        yield return new WaitForSeconds(0.1f);
+//        model.material.color = Color.white;
+//    }
+
+//    void shoot()
+//    {
+//        shootTimer = 0;
+//        Instantiate(bullet, shootPos.position, transform.rotation);
+//    }
+
+//    public void updateenemyUI()
+//    {
+//        gamemanager.instance.enemyHPBar.fillAmount = (float)HP / maxHP;
+//    }
+//}
